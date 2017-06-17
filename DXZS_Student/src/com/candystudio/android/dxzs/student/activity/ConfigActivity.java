6bc845b204cff904b21bbb9665c5c08e632fea90
terrycandy <guocheng@nudt.edu.cn>
2017-06-17 16:00:13 +0800

    本地第三次

--- DXZS_Student/src/com/candystudio/android/dxzs/student/activity/ConfigActivity.java
+++ DXZS_Student/src/com/candystudio/android/dxzs/student/activity/ConfigActivity.java
@@ -9,7 +9,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.nfc.Tag;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
